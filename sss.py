120a66c0de77724975b70eec5a8d7fedd72b7323
kumar shivam <kkumarshivam10@gmail.com>
2025-01-29 19:27:40 +0530

    Add files via upload

---
+++ sss.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd 
+import plotly.express as px 
+import plotly.graph_objects as go 
+import plotly.io as pio  
+import plotly.colors as colors 
+pio.templates.default = "plotly_white"
+
+
+# In[5]:
+
+
+data = pd.read_csv("C:/Users/admin/Downloads/Sample - Superstore (2).csv", encoding='latin-1') 
+
+
+# In[6]:
+
+
+data.head()
+
+
+# # Let’s start by looking at the descriptive statistics of the dataset
+
+# In[7]:
+
+
+data.describe()
+
+
+# In[8]:
+
+
+data.info()
+
+
+# # Converting Date Columns
+
+# In[9]:
+
+
+data['Order Date'] = pd.to_datetime(data['Order Date'])
+data['Ship Date'] = pd.to_datetime(data['Ship Date']) 
+
+
+# # Adding New Date-Based Columns
+
+# In[10]:
+
+
+data['Order Month'] = data['Order Date'].dt.month 
+data['Order Year'] = data['Order Date'].dt.year
+data['Order Day of Week'] = data['Order Date'].dt.dayofweek
+
+
+# In[11]:
+
+
+data.head()
+
+
+# # Monthly Sales Analysis
+
+# In[12]:
+
+
+sales_by_month = data.groupby('Order Month')['Sales'].sum().reset_index()
+fig = px.line(sales_by_month, 
+              x='Order Month', 
+              y='Sales', 
+              title='Monthly Sales Analysis')
+fig.show()
+
+
+# # Sales Analysis by Category
+
+# In[13]:
+
+
+sales_by_category = data.groupby('Category')['Sales'].sum().reset_index()
+
+
+fig = px.pie(sales_by_category, 
+             values='Sales', 
+             names='Category', 
+             hole=0.5, 
+             color_discrete_sequence=px.colors.qualitative.Pastel)
+
+fig.update_traces(textposition='inside', textinfo='percent+label')
+fig.update_layout(title_text='Sales Analysis by Category', title_font=dict(size=24))
+
+fig.show()
+
+
+# # Sales Analysis by Sub-Category
+
+# In[14]:
+
+
+sales_by_subcategory = data.groupby('Sub-Category')['Sales'].sum().reset_index()
+fig = px.bar(sales_by_subcategory, 
+             x='Sub-Category', 
+             y='Sales', 
+             title='Sales Analysis by Sub-Category')
+fig.show()
+
+
+# # Monthly Profit Analysis
+
+# In[15]:
+
+
+profit_by_month = data.groupby('Order Month')['Profit'].sum().reset_index()
+fig = px.line(profit_by_month, 
+              x='Order Month', 
+              y='Profit', 
+              title='Monthly Profit Analysis')
+fig.show()
+
+
+# # Profit Analysis by Category
+
+# In[16]:
+
+
+profit_by_category = data.groupby('Category')['Profit'].sum().reset_index()
+
+fig = px.pie(profit_by_category, 
+             values='Profit', 
+             names='Category', 
+             hole=0.5, 
+             color_discrete_sequence=px.colors.qualitative.Pastel)
+
+fig.update_traces(textposition='inside', textinfo='percent+label')
+fig.update_layout(title_text='Profit Analysis by Category', title_font=dict(size=24))
+
+fig.show()
+
+
+# # Profit Analysis by Sub-Category
+
+# In[17]:
+
+
+sales_profit_by_segment = data.groupby('Segment').agg({'Sales': 'sum', 'Profit': 'sum'}).reset_index()
+
+color_palette = colors.qualitative.Pastel
+
+fig = go.Figure()
+fig.add_trace(go.Bar(x=sales_profit_by_segment['Segment'], 
+                     y=sales_profit_by_segment['Sales'], 
+                     name='Sales',
+                     marker_color=color_palette[0]))
+
+fig.add_trace(go.Bar(x=sales_profit_by_segment['Segment'], 
+                     y=sales_profit_by_segment['Profit'], 
+                     name='Profit',
+                     marker_color=color_palette[1]))
+
+fig.update_layout(title='Sales and Profit Analysis by Customer Segment',
+                  xaxis_title='Customer Segment', yaxis_title='Amount')
+
+fig.show()
+
+
+# # analyse sales-to-profit ratio
+
+# In[18]:
+
+
+sales_profit_by_segment = data.groupby('Segment').agg({'Sales': 'sum', 'Profit': 'sum'}).reset_index()
+sales_profit_by_segment['Sales_to_Profit_Ratio'] = sales_profit_by_segment['Sales'] / sales_profit_by_segment['Profit']
+print(sales_profit_by_segment[['Segment', 'Sales_to_Profit_Ratio']])
+
+
+# In[ ]:
+
+
+
+
